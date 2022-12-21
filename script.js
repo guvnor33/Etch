870e34ca5ccc8ce04ff40f6b831f25bb68c666f3
guvnor33 <30015865+guvnor33@users.noreply.github.com>
2022-12-21 14:30:56 +0700

    Add a toggle "hover" button
    Let's the user draw by simply hovering the mouse (default) or
    draw only while clicking down

--- script.js
+++ script.js
@@ -4,7 +4,86 @@ var gridSize = 32;
 var gridLength = gridSize * gridSize;
 // var gridLength = gridSize * gridSize;
 gridRangeValue.textContent = gridSize;
+var toggle = document.getElementById('toggleHover');
 
+//set up mouse listeners for dragging
+var isDragging = false;
+
+document.addEventListener('mousedown', function(event) {
+  // The mouse has been pressed, so set isDragging to true
+  isDragging = true;
+});
+
+document.addEventListener('mouseup', function(event) {
+  // The mouse has been released, so set isDragging to false
+  isDragging = false;
+});
+
+//Set up the grid
+setupGrid(gridSize);
+
+//pass in the state of the Hover toggle button when calling setupGridSquares
+setupGridSquares(toggle);
+
+//Set up the input range
+var range = document.getElementById("gridRange");
+var gSize = document.getElementById("gridRangeValue");
+gSize.innerHTML = gridSize;
+
+range.onmousemove = function() {
+    gridRangeValue.innerHTML = this.value;
+    gridSize = this.value;
+    console.log('New gridSize is: ' + gridSize);
+}
+
+range.onchange = function() {
+    setupGrid(gridSize);
+    setupGridSquares(toggle);
+}
+
+
+
+
+//Create a node list of buttons
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons);
+buttons.forEach((e) => {
+    e.addEventListener('mousedown', () => {
+        console.log(e.id);
+        console.log(e.classList);
+        buttons.forEach(b => b.classList.remove('active'));
+        if (e.id != 'reset') e.classList.add('active');
+        
+        switch(e.id) {
+            case 'black':
+            drawingMode = 'black';
+            squareBackgroundColor = 'black';
+            break;
+            case 'rainbow':
+            drawingMode = 'rainbow';
+            // code block
+            break;
+            case 'darken':
+            drawingMode = 'darken';
+            // code block
+            break;
+            case 'erase':
+            drawingMode = 'erase';
+            // code block
+            break;
+            case 'reset':
+            drawingMode = 'black';
+            let s = document.querySelector('#black');
+            s.classList.add('active');
+            console.log('Erase the grid, gridlength: ' + gridLength);
+            setupGrid(gridSize);
+            setupGridSquares(toggle);
+            break;
+            default:
+            // code block
+        }
+    })
+});
 
 function setupGrid(size) {
     // calculate the size of one square in the grid
@@ -26,96 +105,44 @@ function setupGrid(size) {
     grid.style.gridTemplateRows = `repeat(${size}, auto)`
 }
 
-//Set up the grid
-setupGrid(gridSize);
-setupGridSquares();
-
-//Set up the input range
-var range = document.getElementById("gridRange");
-var gSize = document.getElementById("gridRangeValue");
-gSize.innerHTML = gridSize;
-
-range.onmousemove = function() {
-    gridRangeValue.innerHTML = this.value;
-    gridSize = this.value;
-    console.log('New gridSize is: ' + gridSize);
-}
-range.onchange = function() {
-    setupGrid(gridSize);
-    setupGridSquares();
-}
-
-function setupGridSquares() {
+function setupGridSquares(toggle) {
     //Create a node list consisting of the individual grid squares
     const gridSquares = document.querySelectorAll(".grid-square");
     gridSquares.forEach((square) => {
-        square.addEventListener('mouseover', () => {
-            switch(drawingMode) {
-                case 'black':
-                square.style.backgroundColor = squareBackgroundColor;
-                break;
-                case 'rainbow':
-                const randomR = Math.floor(Math.random() * 256);
-                const randomG = Math.floor(Math.random() * 256);
-                const randomB = Math.floor(Math.random() * 256);
-                square.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-                break;
-                case 'darken':
-                console.log('darken mode selected')
-                let message = getComputedStyle(square);
-                console.log(message.backgroundColor);
-                square.style.backgroundColor = darkenColor(message.backgroundColor);
-                break;
-                case 'erase':
-                square.style.backgroundColor = 'lightgray';
-                break;
-            };
-        });
+        square.addEventListener('mouseover', (event) => drawHandler(event, square, toggle));
+        square.addEventListener('mousedown', (event) => drawHandler(event, square, toggle));
     });
-}
+};
 
-//Create a node list of buttons
-const buttons = document.querySelectorAll(".btn");
-console.log(buttons);
-buttons.forEach((e) => {
-    e.addEventListener('mousedown', () => {
-        console.log(e.id);
-        console.log(e.classList);
-        buttons.forEach(b => b.classList.remove('active'));
-        if (e.id != 'reset') e.classList.add('active');
-        if (e.id === 'reset') {
-            const s = document.querySelector('#black');
-            s.classList.add('active');
-        }
-        
-        switch(e.id) {
+function drawHandler(event, square, toggle) {
+    let draw = false;
+    console.log('toggle:' + toggle.checked);
+    if ((toggle.checked) && (event.type === 'mouseover')) { draw = true; }
+    if ((toggle.checked === false) && (isDragging)) { draw = true; }
+    // if ((toggle.checked === false) && (event.type === 'mousedown')) { draw = true; }
+    if (draw) {
+        switch(drawingMode) {
             case 'black':
-            drawingMode = 'black';
-            squareBackgroundColor = 'black';
+            square.style.backgroundColor = squareBackgroundColor;
             break;
             case 'rainbow':
-            drawingMode = 'rainbow';
-            // code block
+            const randomR = Math.floor(Math.random() * 256);
+            const randomG = Math.floor(Math.random() * 256);
+            const randomB = Math.floor(Math.random() * 256);
+            square.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
             break;
             case 'darken':
-            drawingMode = 'darken';
-            // code block
+            console.log('darken mode selected')
+            let message = getComputedStyle(square);
+            console.log(message.backgroundColor);
+            square.style.backgroundColor = darkenColor(message.backgroundColor);
             break;
             case 'erase':
-            drawingMode = 'erase';
-            // code block
+            square.style.backgroundColor = 'lightgray';
             break;
-            case 'reset':
-            drawingMode = 'black';
-            console.log('Erase the grid, gridlength: ' + gridLength);
-            setupGrid(gridSize);
-            setupGridSquares();
-            break;
-            default:
-            // code block
         }
-    })
-});
+    }   
+}
 
 // Darken the color of default squares by ~10%
 function darkenColor(rgbColor) {
@@ -143,6 +170,5 @@ function darkenColor(rgbColor) {
         returnColor = 'rgb(0, 0, 0)';
         break;
     }
-    
     return returnColor;
 }
